7a08e6b952fe9d9552edef32e41bace6ac6bce24
Dhanush S <143257334+DhanushSelvarajDev@users.noreply.github.com>
2026-02-28 12:09:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-
-  base: "/template-1/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
 });
+
